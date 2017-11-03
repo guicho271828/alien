ebe96723f8ee90e2ee28fab2aab13cdf108d8eef
Masataro Asai <guicho2.71828@gmail.com>
2017-11-03 15:37:50 +0900

    [ci skip] moved parser-specific util functions to pddl.lisp

--- src/pddl.lisp
+++ src/pddl.lisp
@@ -2,9 +2,21 @@
 
 (in-package :strips)
 
+(defun find-domain (problem-path)
+  (format t "~&finding the domain file...~%")
+  (block nil
+     (let ((dpath (make-pathname :defaults problem-path :name "domain")))
+       (when (probe-file dpath) (return dpath)))
+     (let ((dpath (make-pathname :defaults problem-path :name
+                                 (format nil "~a-domain" (pathname-name problem-path)))))
+       (when (probe-file dpath) (return dpath)))
+     (error "~& Failed to infer the domain pathname from problem pathname!~%Problem: ~a~%Candidate: ~a~%Candidate: ~a"
+            problem-path
+            (make-pathname :defaults problem-path :name "domain")
+            (make-pathname :defaults problem-path :name (format nil "~a-domain" (pathname-name problem-path))))))
+
 (defun parse (problem &optional (domain (find-domain problem)))
   (parse0 domain problem))
-
 
 ;;; parse0
 
--- src/util.lisp
+++ src/util.lisp
@@ -7,15 +7,4 @@
 
 (cl:in-package :strips)
 
-(defun find-domain (problem-path)
-  (format t "~&finding the domain file...~%")
-  (block nil
-     (let ((dpath (make-pathname :defaults problem-path :name "domain")))
-       (when (probe-file dpath) (return dpath)))
-     (let ((dpath (make-pathname :defaults problem-path :name
-                                 (format nil "~a-domain" (pathname-name problem-path)))))
-       (when (probe-file dpath) (return dpath)))
-     (error "~& Failed to infer the domain pathname from problem pathname!~%Problem: ~a~%Candidate: ~a~%Candidate: ~a"
-            problem-path
-            (make-pathname :defaults problem-path :name "domain")
-            (make-pathname :defaults problem-path :name (format nil "~a-domain" (pathname-name problem-path))))))
+
