f07e46cc272afd41a5332e8b8d770df9f1a7a472
Masataro Asai <guicho2.71828@gmail.com>
2017-11-03 15:36:46 +0900

    [ci skip] moved parser-specific util functions to pddl.lisp

--- src/pddl.lisp
+++ src/pddl.lisp
@@ -57,6 +57,87 @@
     (grovel-axioms domain)
     (parse3)))
 
+(defun parse-typed-def (list)
+  "Parse [objs* - type]* list. Does not handle the type inheritance.
+ Returns an alist of parameters and their types.
+ Untyped parameters are given the type OBJECT."
+  (let (db
+        buffer)
+    (labels ((add (s type)
+               (push (cons s type) db))
+             (rec (list)
+               (match list
+                 ((list* '- type rest)
+                  (dolist (s (nreverse buffer))
+                    (add s type))
+                  (setf buffer nil)
+                  (rec rest))
+                 
+                 (nil
+                  (dolist (s buffer)
+                    (add s 'object))
+                  (nreverse db))
+                 
+                 ((list* now rest)
+                  (push now buffer)
+                  (rec rest)))))
+      (rec list))))
+
+(print
+ (parse-typed-def '(hand level beverage dispenser container - object
+                    ingredient cocktail - beverage
+                    shot shaker - container)))
+
+(defun flatten-typed-def (typed-def)
+  "Take a single typed predicate literal L and returns two values:
+the untyped version of L and a list of literals converted from the types of the parameters.
+
+ Example: (?x - table) -> (?x), ((table ?x)) "
+  (let* ((parsed (parse-typed-def typed-def))
+         (w/o-type (mapcar #'car parsed))
+         (type-conditions
+          (iter (for (arg . type) in parsed)
+                (unless (eq type 'object)
+                  (collect `(,type ,arg))))))
+    (values w/o-type type-conditions)))
+
+(defun flatten-types/condition (condition)
+  (ematch condition
+    ((list 'exists params condition)
+     (multiple-value-bind (w/o-type type-conditions) (flatten-typed-def params)
+       `(exists ,w/o-type
+               (and ,@type-conditions
+                    ,(flatten-types/condition condition)))))
+    ((list 'forall params condition)
+     (multiple-value-bind (w/o-type type-conditions) (flatten-typed-def params)
+       `(forall ,w/o-type
+                (imply (and ,@type-conditions)
+                       ,(flatten-types/condition condition)))))
+    ((list* (and kind (or 'and 'or))
+            conditions)
+     `(,kind ,@(mapcar #'flatten-types/condition conditions)))
+    (_ condition)))
+
+(print
+ (flatten-types/condition `(forall (?u - unit) (and (clean)))))
+
+(defun flatten-types/effect (effect)
+  (ematch effect
+    ((list* (and kind (or 'or 'exists)) _)
+     (error "~a should not appear in the effects: ~a" kind effect))
+    (`(forall ,params ,effect)
+     (multiple-value-bind (w/o-type type-conditions) (flatten-typed-def params)
+       `(forall ,w/o-type
+                (when (and ,@type-conditions)
+                  ,(flatten-types/effect effect)))))
+    (`(when ,condition ,body)
+      `(when ,(flatten-types/condition condition)
+         ,(flatten-types/effect body)))
+    (`(and ,@conditions)
+     `(and ,@(mapcar #'flatten-types/effect conditions)))
+    (_ effect)))
+
+
 (defun grovel-types (domain)
   (match domain
     ((assoc :types typed-def)
