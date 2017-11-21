b0841b278efc060f5c4bb6c53e320f5727057905
Masataro Asai <guicho2.71828@gmail.com>
2017-11-21 18:09:43 +0900

    modifiable-fluent-predicates

--- src/4-invariant.lisp
+++ src/4-invariant.lisp
@@ -1,5 +1,57 @@
 
+;;; Invariant synthesys
+
 (in-package :strips)
 
+(defmacro with-parsed-information (info &body body)
+  "Binds the special variables using INFO, which is a parsed & flattened result of pddl files (see 2-translate.lisp).
+   *types* *objects* *predicates* *init* *goal* *axioms* *actions* "
+  `(match ,info
+     ((list :type *types*
+            :objects *objects*
+            :predicates *predicates*
+            :init *init*
+            :goal *goal*
+            :axioms *axioms*
+            :actions *actions*)
+      ,@body)))
 
+(defun preprocess (info)
+  (with-parsed-information info
+    (list* :invarinants (find-invariants)
+            info)))
+
+;;; modifiable-fluent-predicates
+
+(defun modifiable-fluent-predicates ()
+  "Sec 5.1 Initial candidates par.2. Excludes constant and derived predicates"
+  (remove-if (disjoin #'constant-predicate-p
+                      #'derived-predicate-p)
+             *predicates*))
+
+(defun fluent-predicate-p (predicate)
+  (ematch predicate
+    ((list* name _)
+     (iter (for a in *actions*)
+           (ematch a
+             ((plist :effect `(and ,@effects))
+              (iter (for e in effects)
+                    (match e
+                      (`(forall ,_ (when ,_ (,(eq name) ,@_)))
+                        (return-from fluent-predicate-p t))
+                      (`(forall ,_ (when ,_ (not (,(eq name) ,@_))))
+                        (return-from fluent-predicate-p t))))))))))
+
+(defun constant-predicate-p (predicate)
+  (not (fluent-predicate-p predicate)))
+
+(defun derived-predicate-p (predicate)
+  (ematch predicate
+    ((list* name _)
+     (iter (for a in *axioms*)
+           (match a
+             (`(:derived (,(eq name) ,@_) ,@_)
+               (return-from derived-predicate-p t)))))))
+  
+;;; initial candidates
 
