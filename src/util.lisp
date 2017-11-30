bd686feebd258130f5a488ecd9eafc5b9fcc5cc9
Masataro Asai <guicho2.71828@gmail.com>
2017-11-30 09:56:37 +0900

    defun* --- maybe not used

--- src/util.lisp
+++ src/util.lisp
@@ -7,4 +7,42 @@
 
 (cl:in-package :strips)
 
+(defmacro defun* (name args &body body)
+  (match name
+    ((or (and (symbol) name       (<> return-type '*))
+         (and (list 'setf _) name (<> return-type '*))
+         (and (list name return-type))
+         (and (list (and (list 'setf _) name) return-type)))
+     (let ((required (or (position-if (lambda (elem) (member elem '(&optional &key &rest &aux))) args)
+                         (length args))))
+       (iter (for (arg . rest) on args)
+             (repeat required)
+             (match arg
+               ((list arg type)
+                (collecting arg into argsyms)
+                (collecting type into types))
+               (_
+                (collecting arg into argsyms)
+                (collecting '* into types)))
+             (finally
+              (let* ((rest-args (nthcdr required args))
+                     (rest-types (iter (for elem in rest-args)
+                                       (collecting
+                                        (if (member elem '(&optional &key &rest &aux))
+                                            elem
+                                            '*)))))
+                (return
+                  `(progn
+                     (declaim (ftype (function (,@types ,@rest-types) ,return-type) ,name))
+                     (defun ,name ,(append argsyms rest-args)
+                       ,@body))))))))))
+
+#+(or)
+(progn
+  (defun* fn (a b c &rest rest)
+    (list (+ a b c) rest))
+  (defun* (fn list) (a b c &rest rest)
+    (list (+ a b c) rest))
+  (defun* (fn list) ((a fixnum) b c &rest rest)
+    (list (+ a b c) rest)))
 
