280780bbd1da86b67ecd7df5622e9f79729404f1
Masataro Asai <guicho2.71828@gmail.com>
2017-12-23 16:48:26 +0900

    testing fast downward translator speed on moderately difficult instance

---
+++ test.sh
@@ -0,0 +1,6 @@
+
+# testing fast downward speed on moderately difficult instance
+
+../mwup/downward/src/translate/translate.py axiom-domains/opttel-adl-derived/domain.pddl axiom-domains/opttel-adl-derived/p30.pddl
+
+# Done! [3.270s CPU, 3.299s wall-clock]
