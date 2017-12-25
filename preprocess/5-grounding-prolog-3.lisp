93f26247bef7e2dc28be55dda653fee2b36ba210
Masataro Asai <guicho2.71828@gmail.com>
2017-12-25 12:29:24 +0900

    added reachable-fact for =

--- preprocess/5-grounding-prolog-3.lisp
+++ preprocess/5-grounding-prolog-3.lisp
@@ -101,7 +101,8 @@ This is a rewrite of 5-grounding-prolog with minimally using the lifted predicat
      `((:- (reachable-fact ?f)
            (reachable-axiom ?f))
        (:- (reachable-fact ?f)
-           (reachable-effect ?f)))
+           (reachable-effect ?f))
+       (reachable-fact (= ?o ?o)))
      (iter (for a in *actions*)
            (ematch a
              ((plist :action name
