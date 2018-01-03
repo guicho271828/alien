178f3c15adaa9b7e2f7524b4683b1ce4646c0b33
Masataro Asai <guicho2.71828@gmail.com>
2018-01-03 09:26:44 +0900

    implemented flags for negative preconditions

--- preprocess/specials.lisp
+++ preprocess/specials.lisp
@@ -3,3 +3,9 @@
 
 (defvar *enable-no-op-pruning* nil)
 
+(defvar *enable-negative-precondition-pruning-for-fluents* nil)
+(defvar *enable-negative-precondition-pruning-for-axioms* nil)
+
+(define-symbol-macro *enable-negative-precondition-pruning*
+    (or *enable-negative-precondition-pruning-for-axioms*
+        *enable-negative-precondition-pruning-for-fluents*))
