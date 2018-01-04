12e6fee258a69edb8cef98d0f7089f7e0f7c80b3
Masataro Asai <guicho2.71828@gmail.com>
2018-01-05 08:52:45 +0900

    index-entries

--- preprocess/lib/package.lisp
+++ preprocess/lib/package.lisp
@@ -1,5 +1,10 @@
 
 (defpackage :strips.lib
-  (:use :cl :trivia :iterate :alexandria))
-
+  (:use :cl :trivia :iterate :alexandria)
+  (:export
+   #:make-index
+   #:index-insert
+   #:index
+   #:index-ref
+   #:index-size))
 
