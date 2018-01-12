ec0ed38352de0d6289cd7a10250e6061be96d6d0
Masataro Asai <guicho2.71828@gmail.com>
2018-01-13 00:44:19 +0900

    quick n dirty open list

--- search/open-list.lisp
+++ search/open-list.lisp
@@ -5,9 +5,24 @@
 
 (defvar *open-list*)
 
-
-
-
 (defstruct bucket-open-list
-  (buckets (make-array 32 :element-type '(array))))
+  "bucket open list with single key, fifo tiebreaking; initial, slow implementation"
+  (min-key 0 :type fixnum)
+  (buckets (make-array 32 :element-type 'list :initial-element nil :adjustable t)))
 
+(defun bucket-open-list-insert (open key element)
+  (match open
+    ((bucket-open-list buckets :min-key (place min-key))
+     (unless (array-in-bounds-p buckets key)
+       (adjust-array buckets (* 2 (array-total-size buckets))))
+     (push element (aref buckets key))
+     (minf min-key key))))
+
+(defun bucket-open-list-pop (open)
+  (match open
+    ((bucket-open-list buckets :min-key (place min-key-place min-key))
+     (prog1 (pop (aref buckets min-key))
+       (do ()
+           ((aref buckets min-key)
+            (setf min-key-place min-key))
+         (incf min-key))))))
