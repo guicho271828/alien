d3b9ece0a86355f8f965b7dfd70f87914e84aafd
Masataro Asai <guicho2.71828@gmail.com>
2018-01-19 12:54:26 +0900

    memory-usage

--- search/0-util.lisp
+++ search/0-util.lisp
@@ -11,3 +11,48 @@
 
 (defun decode-op (op)
   (strips.lib:index-ref *op-index* op))
+
+(defun memory-usage ()
+  (sb-ext:gc :full t)
+  (sb-vm:memory-usage :print-spaces t :count-spaces '(:dynamic) :print-summary nil))
+
+;; (defstruct a b c)
+
+;; 384
+;; 352
+;; _32
+;; 
+;; 1 fixnum: 8 bytes
+;; i.e. 4 slots per struct
+
+;; (defstruct (v (:type vector)) b c)
+;; (defstruct (v2 (:type vector))
+;;   (b 0 :type fixnum)
+;;   (c 0 :type fixnum))
+
+;; simple-vector also consumes 32 bytes
+;; additional 2 words for each structure
+
+;; (defstruct (v3 (:type (array fixnum)))
+;;   (b 0 :type fixnum)
+;;   (c 0 :type fixnum))
+
+;; (defun v2-b* (v2)
+;;   (declare (vector v2)
+;;            (optimize (safety 0) (speed 3) (debug 0)))
+;;   (v2-b v2))
+;; 
+;; (defun v2-c* (v2)
+;;   (declare (vector v2)
+;;            (optimize (safety 0) (speed 3) (debug 0)))
+;;   (v2-c v2))
+;; 
+;; (defun v-b* (v)
+;;   (declare (vector v)
+;;            (optimize (safety 0) (speed 3) (debug 0)))
+;;   (v-b v))
+;; 
+;; (defun v-c* (v)
+;;   (declare (vector v)
+;;            (optimize (safety 0) (speed 3) (debug 0)))
+;;   (v-c v))
