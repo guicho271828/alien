5c09e10b9cdcfd1661a381a0bc7dac4bc4765b21
Masataro Asai <guicho2.71828@gmail.com>
2018-01-27 22:28:01 +0900

    alien_plan result is wrong

--- demo/parkprinter/run.sh
+++ demo/parkprinter/run.sh
@@ -2,7 +2,8 @@
 
 ../../downward/fast-downward.py --run-all --alias lama-first domain.pddl p00.pddl
 
-../../downward/builds/release32/bin/validate -v domain.pddl p00.pddl p00.sas_plan
+../../downward/builds/release32/bin/validate -v domain.pddl p00.pddl p00.sas_plan ; echo $?
+../../downward/builds/release32/bin/validate -v domain.pddl p00.pddl p00.alien_plan ; echo $?
 
 # there was a problem in adding & deleting the same literal
 
