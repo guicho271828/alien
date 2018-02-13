b4d6346ddef030edb74b037adc045f6f85cc41af
Masataro Asai <guicho2.71828@gmail.com>
2018-02-14 00:52:33 +0900

    remove word-offset: not gonna use it

--- lib/packed-struct.lisp
+++ lib/packed-struct.lisp
@@ -127,17 +127,14 @@
 ;; treating a big vector as a large integer and retrieve the value
 
 (declaim (inline %packed-accessor-int))
-(defun %packed-accessor-int (vector size position &optional (word-offset 0))
+(defun %packed-accessor-int (vector size position)
   "offset: number of bits from the beginning of the structure
 size: number of bits for the structure"
   (declare (fixnum position)
-           (fixnum word-offset)
            ((integer 0 64) size)
            (simple-bit-vector vector))
   (multiple-value-bind (index-begin offset-begin) (floor position 64)
-    (incf index-begin word-offset)
     (multiple-value-bind (index-end offset-end) (floor (+ size position) 64)
-      (incf index-end word-offset)
       (cond
         ((or (= index-begin index-end)
              (= 0 offset-end))
@@ -152,18 +149,15 @@ size: number of bits for the structure"
                       (- 64 offset-begin)))))))))
 
 (declaim (inline (setf %packed-accessor-int)))
-(defun (setf %packed-accessor-int) (newval vector size position &optional (word-offset 0))
+(defun (setf %packed-accessor-int) (newval vector size position)
   "position: number of bits from the beginning of the structure
 size: number of bits for the structure"
   (declare (fixnum position)
-           (fixnum word-offset)
            ((integer 0 64) size)
            ((unsigned-byte 64) newval)
            (simple-bit-vector vector))
   (multiple-value-bind (index-begin offset-begin) (floor position 64)
-    (incf index-begin word-offset)
     (multiple-value-bind (index-end offset-end) (floor (+ size position) 64)
-      (incf index-end word-offset)
       (cond
         ((or (= index-begin index-end)
              (= 0 offset-end))
