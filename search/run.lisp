13c794a9842a7629810c02edb1f5ff5bde84763c
Masataro Asai <guicho2.71828@gmail.com>
2018-02-13 22:24:36 +0900

    compile/load/clear-system

--- search/run.lisp
+++ search/run.lisp
@@ -1,9 +1,11 @@
 (in-package :strips)
-(named-readtables:in-readtable :fare-quasiquote)
 
 (defun run (search-engine)
   (match search-engine
     ((searcher storage body)
      (eval `(define-packed-struct state-information (,storage)))
+     (asdf:compile-system :strips.instance-dependent :force t)
+     (asdf:load-system :strips.instance-dependent :force t)
+     ;; ensure the specialised code is removed and does not affect the later debugging
+     (asdf:clear-system :strips.instance-dependent)
      (funcall (compile nil `(lambda () ,body))))))
-
