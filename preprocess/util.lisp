714f59ab12967091d0737ee8a9fc6e9ad208d768
Masataro Asai <guicho2.71828@gmail.com>
2018-02-15 10:47:45 +0900

    break+

--- preprocess/util.lisp
+++ preprocess/util.lisp
@@ -243,3 +243,16 @@
 
 (defun println (x)
   (write x :escape nil) (terpri))
+
+(defmacro break+ (&rest args)
+  (let* ((last-form (lastcar args))
+         (last last-form)
+         (butlast (butlast args)))
+    (once-only (last)
+      `(progn
+         (break "~@{~a~2%~<;;~@; result:~4i~:@_~a~;~:>~2%~}"
+                ,@(iter (for arg in butlast)
+                        (collect `',arg)
+                        (collect `(list ,arg)))
+                ',last-form (list ,last))
+         ,last))))
