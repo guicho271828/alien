98e0b8d56be78e18d6176da407500f26e2b02c41
Masataro Asai <guicho2.71828@gmail.com>
2018-02-25 13:06:02 +0900

    fix bucket-open-list

--- search/bucket-open-list.lisp
+++ search/bucket-open-list.lisp
@@ -6,31 +6,29 @@
 ;; slow, initial implementation of bucket open list as a placeholder for initial submission
 
 (defstruct bucket-open-list
-  "bucket open list with single key, fifo tiebreaking; initial, slow implementation"
-  (min-key 32 :type array-index)
+  "bucket open list with a single key, lifo tiebreaking; initial, slow implementation."
+  (min-key most-positive-fixnum :type fixnum) ; initially out-of-bounds
   (buckets (make-array 32 :element-type 'list :initial-element nil :adjustable t)))
 
+;; the state of min-key is either out-of-bounds or actual.
+
 (defun bucket-open-list-insert (open key element)
-  (match open
+  (ematch open
     ((bucket-open-list buckets :min-key (place min-key))
      (unless (array-in-bounds-p buckets key)
-       (adjust-array buckets (max (length buckets) (expt 2 (integer-length key)))
-                     :initial-element nil))
+       (adjust-array buckets (expt 2 (integer-length key)) :initial-element nil))
      (push element (aref buckets key))
      (minf min-key key))))
 
 (defun bucket-open-list-pop (open)
   (ematch open
-    ((bucket-open-list buckets :min-key (place min-key-place min-key))
-     (when (not (array-in-bounds-p buckets min-key))
+    ((bucket-open-list buckets :min-key (place min-key))
+     (when (= min-key most-positive-fixnum) 
        (error 'no-solution))
      (prog1 (pop (aref buckets min-key))
-       (do ()
-           ((if (array-in-bounds-p buckets min-key)
-                (aref buckets min-key)
-                t)
-            (setf min-key-place min-key))
-         (incf min-key))))))
+       (if-let ((pos (position-if #'identity buckets :start min-key)))
+         (setf min-key pos)
+         (setf min-key most-positive-fixnum))))))
 
 (defun bucket-open-list (evaluator)
   (ematch evaluator
