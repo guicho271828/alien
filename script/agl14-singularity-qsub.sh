2a1d25aa608ce851950aa2c741f109cd075fdc85
Masataro Asai <guicho2.71828@gmail.com>
2018-03-06 10:41:39 +0900

    [qsub] qsub

--- script/agl14-singularity-qsub.sh
+++ script/agl14-singularity-qsub.sh
@@ -2,7 +2,8 @@
 
 dir=$(readlink -ef $(dirname $0))
 export SHELL=bash
-export options="-m 6000 -t 300 --search-option '(eager (bucket-open-list (novelty :k 5)))'"
+
+export options="-m 6000 -t 300 --search-option '(eager (bucket-open-list (blind)))'"
 
 parallel qsub \
          -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
@@ -10,4 +11,24 @@ parallel qsub \
          -o /dev/null \
          -v options,problem={} \
          $dir/singularity-alien.sh ::: \
-         $(ls ipc2011-opt/*/*.pddl | grep -v domain )
+         $(ls ipc2011-opt-blind/*/*.pddl | grep -v domain )
+
+export options="-m 6000 -t 300 --search-option '(eager (bucket-open-list (ff/rpg)))'"
+
+parallel qsub \
+         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
+         -e /dev/null \
+         -o /dev/null \
+         -v options,problem={} \
+         $dir/singularity-alien.sh ::: \
+         $(ls ipc2011-opt-ff/*/*.pddl | grep -v domain )
+
+export options="-m 6000 -t 300 --search-option '(eager (bucket-open-list (sum (product (constant (expt 2 (ceiling (log strips::*op-size* 2)))) (novelty4)) (ff/rpg))))'"
+
+parallel qsub \
+         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
+         -e /dev/null \
+         -o /dev/null \
+         -v options,problem={} \
+         $dir/singularity-alien.sh ::: \
+         $(ls ipc2011-opt-bwfs/*/*.pddl | grep -v domain )
