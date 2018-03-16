c8549dabfaf05b8719c048d14e71d32ad4f0d16e
Masataro Asai <guicho2.71828@gmail.com>
2018-03-16 10:41:39 +0900

    [optimize] run do-leaf only once

--- search-instance-dependent/alien-ffish.lisp
+++ search-instance-dependent/alien-ffish.lisp
@@ -27,8 +27,8 @@ and count the number of reaching the semi-relaxed goal.
 (defun alien ()
   (push 'alien/rpg *optional-features*)
   (push 'ff/rpg *optional-features*)
-  (ensure-delete-relaxed-sg)
-  (ensure-delete-only-sg)
+  (ensure-delete-relaxed-sg nil)
+  (ensure-delete-only-sg nil)
   (make-evaluator
    :storage '()
    :function '(function alien-heuristics/rpg)))
@@ -56,56 +56,58 @@ and count the number of reaching the semi-relaxed goal.
         (replace state-db state)
         (replace state1 state)
         (replace state2 state)
-        (iter
-           (for step from 0)
-           (let ((applied nil))
-             (do-leaf (op-id state1 *delete-relaxed-sg*)
-               (unless (= 1 (aref op-db op-id)) ;else, already applied
-                 (setf (aref op-db op-id) 1
-                       applied t)
-                 (delete-relaxed-apply-op op-id state1 state2)))
-             (unless applied
-               ;; no new action applied; fail
-               ;; (log:info "took ~a steps (failed, no action)" step)
-               (return)))
-           (apply-axioms state2)
-           (when (goalp state2)
-             (incf success)
-             ;; (log:info "took ~a steps (success)" step)
-             (return))
+        (iter (for step from 0)
+              (let ((ops (load-time-value (make-array *op-size* :element-type 'op-id)))
+                    (c 0))
+                (do-leaf (op-id state1 *delete-relaxed-sg*)
+                  (unless (= 1 (aref op-db op-id)) ;else, already applied
+                    (setf (aref op-db op-id) 1
+                          (aref ops c) op-id)
+                    (incf c)))
+                (when (zerop c)
+                  ;; no new action applied; fail
+                  ;; (log:info "took ~a steps (failed, no action)" step)
+                  (return))
+                (iter (for (the op-id op-id) in-vector ops below c)
+                      (delete-relaxed-apply-op op-id state1 state2))
+                (apply-axioms state2)
+                (when (goalp state2)
+                  (incf success)
+                  ;; (log:info "took ~a steps (success)" step)
+                  (return))
 
-           (let ((achieved (make-state+axioms)))
-             (declare (dynamic-extent achieved))
-             (bit-andc1 state-db state2 achieved) ; 1 when 0 in db and 1 in child
-             (when (not (find 1 achieved))
-               ;; no new proposition added; fail
-               ;; (log:info "took ~a steps (failed, no new proposition)" step)
-               (return)))
-           (bit-ior state2 state-db state-db)
-           
-           ;; apply all deletes
-           (replace state3 state2)
-           (do-leaf (op-id state1 *delete-only-sg*) ; the use of state1 is not a mistake.
-             (delete-only-apply-op op-id state1 state3))
+                (let ((achieved (make-state+axioms)))
+                  (declare (dynamic-extent achieved))
+                  (bit-andc1 state-db state2 achieved) ; 1 when 0 in db and 1 in child
+                  (when (not (find 1 achieved))
+                    ;; no new proposition added; fail
+                    ;; (log:info "took ~a steps (failed, no new proposition)" step)
+                    (return)))
+                (bit-ior state2 state-db state-db)
+                
+                ;; apply all deletes
+                (replace state3 state2)
+                (iter (for (the op-id op-id) in-vector ops below c)
+                      (delete-only-apply-op op-id state1 state3))
 
-           ;; restore some deletes
-           (let ((deletes (make-state+axioms))
-                 (random  (make-state+axioms)))
-             (declare (dynamic-extent deletes random))
-             (bit-andc2 state2 state3 deletes)
-             ;; smaller *semi-relaxed-rate-log2* = more 1s in RANDOM.
-             ;; *semi-relaxed-rate-log2* = 0 is equivalent to delete-relaxation, and meaningless.
-             (set/reset-random-bitvector^2 (maybe-inline-obj *semi-relaxed-rate-log2*) random)
-             ;; choose which deletes to relax.
-             (bit-and deletes random deletes)
-             ;; restore some deletes
-             (bit-ior state3 deletes state3))
+                ;; restore some deletes
+                (let ((deletes (make-state+axioms))
+                      (random  (make-state+axioms)))
+                  (declare (dynamic-extent deletes random))
+                  (bit-andc2 state2 state3 deletes)
+                  ;; smaller *semi-relaxed-rate-log2* = more 1s in RANDOM.
+                  ;; *semi-relaxed-rate-log2* = 0 is equivalent to delete-relaxation, and meaningless.
+                  (set/reset-random-bitvector^2 (maybe-inline-obj *semi-relaxed-rate-log2*) random)
+                  ;; choose which deletes to relax.
+                  (bit-and deletes random deletes)
+                  ;; restore some deletes
+                  (bit-ior state3 deletes state3))
 
-           (fill state3 0 :start (maybe-inline-obj *fact-size*))
-           (apply-axioms state3)
-           ;; prepare for the next iteration
-           (replace state1 state3)
-           (replace state2 state3)))
+                (fill state3 0 :start (maybe-inline-obj *fact-size*))
+                (apply-axioms state3)
+                ;; prepare for the next iteration
+                (replace state1 state3)
+                (replace state2 state3))))
       ;; (log:info "~a / ~a success." success *probe-limit*)
       (- (maybe-inline-obj *probe-limit*) success)))
   (print-function-size 'alien-heuristics/rpg))
