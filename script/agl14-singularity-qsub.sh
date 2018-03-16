9b06a3c481822f3d5cd9b5ff5d41cb8cc2494276
Masataro Asai <guicho2.71828@gmail.com>
2018-03-16 11:46:12 +0900

    [qsub] new qsub scripts

--- script/agl14-singularity-qsub.sh
+++ script/agl14-singularity-qsub.sh
@@ -3,52 +3,30 @@
 dir=$(readlink -ef $(dirname $0))
 export SHELL=bash
 
-export options="--alias alien3"
+export options
+export probdir
 
-parallel qsub \
-         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
-         -e /dev/null \
-         -o /dev/null \
-         -v options,problem={} \
-         $dir/singularity-alien.sh ::: \
-         $(ls ipc2014-agl-alien3/*/*.pddl | grep -v domain )
+run (){
+    parallel qsub \
+             -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
+             -e /dev/null \
+             -o /dev/null \
+             -v options,problem={} \
+             $dir/singularity-alien.sh ::: \
+             $(ls $probdir/*/*.pddl | grep -v domain )
+}
 
-export options="--alias alien2"
+options="--alias alien3"     probdir="../results3/ipc2014-agl-alien3" run
+options="--alias alien2"     probdir="../results3/ipc2014-agl-alien2" run
+options="--alias alien"      probdir="../results3/ipc2014-agl-alien"  run
+options="--alias ff"         probdir="../results3/ipc2014-agl-ff"     run
+options="--alias blind"      probdir="../results3/ipc2014-agl-blind"  run
+options="--alias goal-count" probdir="../results3/ipc2014-agl-gc"     run
+options="--alias bwfs2"      probdir="../results3/ipc2014-agl-bwfs2"  run
+options="--alias bwfs3"      probdir="../results3/ipc2014-agl-bwfs3"  run
+options="--alias bwfs4"      probdir="../results3/ipc2014-agl-bwfs4"  run
 
-parallel qsub \
-         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
-         -e /dev/null \
-         -o /dev/null \
-         -v options,problem={} \
-         $dir/singularity-alien.sh ::: \
-         $(ls ipc2014-agl-alien2/*/*.pddl | grep -v domain )
+options="--alias alien4"     probdir="../results3/ipc2014-agl-alien4" run
 
-export options="--alias alien"
 
-parallel qsub \
-         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
-         -e /dev/null \
-         -o /dev/null \
-         -v options,problem={} \
-         $dir/singularity-alien.sh ::: \
-         $(ls ipc2014-agl-alien/*/*.pddl | grep -v domain )
 
-export options="--alias ff"
-
-parallel qsub \
-         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
-         -e /dev/null \
-         -o /dev/null \
-         -v options,problem={} \
-         $dir/singularity-alien.sh ::: \
-         $(ls ipc2014-agl-ff/*/*.pddl | grep -v domain )
-
-export options="--alias bwfs"
-
-parallel qsub \
-         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
-         -e /dev/null \
-         -o /dev/null \
-         -v options,problem={} \
-         $dir/singularity-alien.sh ::: \
-         $(ls ipc2014-agl-bwfs/*/*.pddl | grep -v domain )
