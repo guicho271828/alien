baf9d0dec4ab308ed1e420907c030764aaf4380d
Masataro Asai <guicho2.71828@gmail.com>
2018-03-16 16:57:46 +0900

    [qsub] probe

---
+++ script/qsub-probe.sh
@@ -0,0 +1,45 @@
+#!/bin/bash +x
+
+[ -z $PBS_O_WORKDIR ] || cd $PBS_O_WORKDIR
+
+tmp=$(mktemp -d)
+echo $tmp
+chmod 777 $tmp
+
+export PATH=/usr/local/bin:$PATH
+
+echo "problem: ${problem:=$1}"
+echo "options: ${options:=$2}"
+
+dir=$(dirname $problem)
+
+basename=$dir/$(basename $problem .pddl)
+
+
+if [ -f $dir/domain.pddl ]
+then
+    domain=$dir/domain.pddl
+elif [ -f $basename-domain.pddl ]
+then
+    domain=$basename-domain.pddl
+else
+    echo "no domain file!" >&2
+    exit 1
+fi
+
+plan=$basename.plan
+out=$basename.out
+err=$basename.err
+
+
+t_problem=$tmp/$(basename $problem)
+t_domain=$tmp/$(basename $domain)
+t_plan=$tmp/$(basename $plan)
+t_err=$tmp/$(basename $err)
+t_out=$tmp/$(basename $out)
+
+cp -t $tmp $problem $domain
+
+trap "cp -u -t $dir $t_out $t_err $t_plan; rm -r $tmp" exit
+
+planner-scripts/limit.sh -v -- probe-clean $t_problem > $t_out 2> $t_err
