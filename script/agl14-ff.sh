f6f4c58aa9d1f378d54e80d6fe0404747c72cb0b
Masataro Asai <guicho2.71828@gmail.com>
2018-03-16 15:50:15 +0900

    [qsub] standard ff runner

---
+++ script/agl14-ff.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+dir=$(readlink -ef $(dirname $0))
+export SHELL=bash
+
+export probdir="../results3/ipc2014-agl-fforig"
+
+parallel qsub \
+         -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
+         -e /dev/null \
+         -o /dev/null \
+         -v problem={} \
+         $dir/qsub-ff.sh ::: \
+         $(ls $probdir/*/*.pddl | grep -v domain )
