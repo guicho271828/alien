e42f5e40265b3b6450c298a50fc0f2e4bea529ba
Masataro Asai <guicho2.71828@gmail.com>
2018-03-21 17:24:40 +0900

    updated qsub

--- script/agl14-singularity-qsub.sh
+++ script/agl14-singularity-qsub.sh
@@ -6,7 +6,10 @@ export SHELL=bash
 export options
 export probdir
 
+origdir=$dir/../../results3/ipc2014-agl
+
 run (){
+    cp -r $origdir $probdir
     parallel qsub \
              -l "mem=8000mb,pmem=8000mb,cput=300,walltime=300" \
              -e /dev/null \
@@ -16,17 +19,24 @@ run (){
              $(ls $probdir/*/*.pddl | grep -v domain )
 }
 
-options="--alias alien3"     probdir="../results3/ipc2014-agl-alien3" run
-options="--alias alien2"     probdir="../results3/ipc2014-agl-alien2" run
-options="--alias alien"      probdir="../results3/ipc2014-agl-alien"  run
+options="--alias alien"             probdir="../results3/ipc2014-agl-alien"              run
+options="--alias alien+ff"          probdir="../results3/ipc2014-agl-alien+ff"           run
+options="--alias novelty2+alien+ff" probdir="../results3/ipc2014-agl-novelty2+alien+ff"  run
+options="--alias novelty3+alien+ff" probdir="../results3/ipc2014-agl-novelty3+alien+ff"  run
+
+options="--alias alien/rpg"             probdir="../results3/ipc2014-agl-alien_rpg"              run
+options="--alias alien/rpg+ff"          probdir="../results3/ipc2014-agl-alien_rpg+ff"           run
+options="--alias novelty2+alien/rpg+ff" probdir="../results3/ipc2014-agl-novelty2+alien_rpg+ff"  run
+options="--alias novelty3+alien/rpg+ff" probdir="../results3/ipc2014-agl-novelty3+alien_rpg+ff"  run
+
+options="--alias alieni"            probdir="../results3/ipc2014-agl-alieni"             run
+options="--alias alieni/rpg"            probdir="../results3/ipc2014-agl-alieni_rpg"             run
+
+
 options="--alias ff"         probdir="../results3/ipc2014-agl-ff"     run
 options="--alias blind"      probdir="../results3/ipc2014-agl-blind"  run
 options="--alias goal-count" probdir="../results3/ipc2014-agl-gc"     run
 options="--alias bwfs2"      probdir="../results3/ipc2014-agl-bwfs2"  run
 options="--alias bwfs3"      probdir="../results3/ipc2014-agl-bwfs3"  run
 options="--alias bwfs4"      probdir="../results3/ipc2014-agl-bwfs4"  run
-
-options="--alias alien4"     probdir="../results3/ipc2014-agl-alien4" run
-
-
 
